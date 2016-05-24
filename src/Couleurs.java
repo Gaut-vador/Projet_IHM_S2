4092f9137781d0f8fa359bdb7b10da40642e6f1f
Gaut-vador <gauthier.pirlet@gmail.com>
2016-05-24 16:16:31 +0200

    code couleur

--- src/Couleurs.java
+++ src/Couleurs.java
@@ -1,23 +1,41 @@
 import java.awt.Color;
+import java.util.HashMap;
+import java.util.Random;
 
 public class Couleurs {
 
-	NuanceDeGris nuanceDeGris;
-	
+	HashMap<Integer, Color> nuancier = new HashMap<>();
+	int nbCouleur;
 
-	Couleurs() {
+	Couleurs(int r, int g, int b, int nbCouleur) {
+		this.nbCouleur = nbCouleur;
+		nuancier.put(convertToNuance(new Color(r, g, b)), new Color(r, g, b));
+		genererNuanceGris(nbCouleur);
 	}
 
-	Couleurs(int r, int g, int b, int nbcouleur) {	
-		nuanceDeGris = new NuanceDeGris(nbcouleur);
-		this.nuanceDeGris.setNuance(nuanceDeGris.convertToNuance(new Color(r, g, b)), new Color(r, g, b));	
+	public int convertToNuance(Color c) {
+		return (int) (0.2126 * c.getRed() + 0.7152 * c.getGreen() + 0.0722 * c.getBlue());
 	}
-	
-	Couleurs(int r, int g, int b) {	
-		
+
+	void genererNuanceGris(int nb) {
+		Random rng = new Random();
+		int[] alea = new int[this.nbCouleur];
+		do {
+			for (int i = 1; i <= alea.length; i++) {
+				alea[i] = rng.nextInt();
+			}
+		} while (!verifAlea(alea));
+
 	}
-	
-	
-	
+
+	boolean verifAlea(int[] tab) {
+		for (int i = 0; i < tab.length - 1; i++) {
+			for (int j = i; j < tab.length - 1; j++) {
+				if ((tab[j] > (tab[j + 1] + 10) || tab[j] < (tab[j + 1] - 10)))
+					return false;
+			}
+		}
+		return true;
+	}
 
 }
