14dc7827a58973fbf396b89622e1f3a00c448344
Pirlet Gauthier <gauthier.pirlet@gmail.com>
2016-06-03 14:52:48 +0200

    Add files via upload

--- src/Couleurs.java
+++ src/Couleurs.java
@@ -6,39 +6,56 @@ import java.util.Map.Entry;
 public class Couleurs {
 
 	HashMap<Color, Integer> nuancier = new HashMap<>();
+
 	int nbCouleur;
 
 	Couleurs() {
 		for (int r = 0; r < 256; r++) {
-			for (int v = 0; v < 256; r++) {
-				for (int b = 0; b < 256; r++) {
+			for (int v = 0; v < 256; v++) {
+				for (int b = 0; b < 256; b++) {
 					nuancier.put(new Color(r, v, b), convertToNuance(new Color(r, v, b)));
 				}
 			}
 		}
+		// System.out.println("Couleurs");
 	}
 
 	public int convertToNuance(Color c) {
 		return (int) (0.2126 * c.getRed() + 0.7152 * c.getGreen() + 0.0722 * c.getBlue());
 	}
 
-	public ArrayList[] genererCouleur(int nbCouleur, Color couleur) {
-		
-		ArrayList<Color> tab[] = new ArrayList[nbCouleur];
-		int tmp = convertToNuance(couleur);
-		for (int i = 0; i < nbCouleur; i++) {
-			for (ArrayList<Color> A : tab) {
-				A = new ArrayList<>();
-				for (Entry<Color, Integer> e : nuancier.entrySet()) {
-					if (e.getValue() == tmp) {
-						A.add(e.getKey());
-					}
-				}
-			}
+	public Color[][] genererCouleur(int nbCouleur, Color couleur) {
 
-			tmp = (tmp + 10) % 255;
+		// System.out.println("genererCouleur");
+
+		Color[][] tab = new Color[nbCouleur][500];
+		int cpt = 0;
+		int cptErreur = 0;
+
+		int tmp = convertToNuance(couleur);
+
+		for (int i = 0; i < nbCouleur; i++) {
+
+			for (Entry<Color, Integer> e : nuancier.entrySet()) {
+				if (e.getValue() == tmp && cptErreur % 1000 == 0) {
+					tab[i][cpt] = e.getKey();
+					// System.out.println(tab[i][cpt] + "\t"+cpt);
+
+					cpt++;
+
+				}
+				cptErreur++;
+
+			}
+			cpt = 0;
+
+			if ((tmp + 10) % 255 == 0)
+				tmp = 10;
+			else
+				tmp = (tmp + 10) % 255;
 		}
 
+		// System.out.println("genererCouleur 2");
 		return tab;
 	}
 
